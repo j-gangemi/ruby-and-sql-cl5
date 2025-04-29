fd60e312f6c10eef4d0b7430fda511b3f1cd780c
Julia Gangemi <julia.gangemi@kellogg.northwestern.edu>
2025-04-29 16:57:13 +0000

    started working on associations

--- labs/3-associations.rb
+++ labs/3-associations.rb
@@ -16,9 +16,29 @@ Activity.destroy_all
 
 # 1. insert 3 rows in the activities table with relationships to
 # a single salesperson and 2 different contacts
+apple = Company.find_by({"name" => "Apple"})
+amazon = Company.find_by({"name" => "Amazon"})
+apple_contacts = Contact.where({"company_id" => apple["id"]})
+amazon_contacts = Contact.where({"company_id" => amazon["id"]})
+
 
 activity = Activity.new
+activity["salesperson_id"] =
+activity["contact_id"] =
+activity["note"] = "quick checkin over facetime"
+activity.save
 
+activity = Activity.new
+activity["salesperson_id"] =
+activity["contact_id"] =
+activity["note"] = "met at Cupertino"
+activity.save
+
+activity = Activity.new
+activity["salesperson_id"] =
+activity["contact_id"] =
+activity["note"] = "met at Blue Origin HQ"
+activity.save
 
 # 2. Display all the activities between the salesperson used above
 # and one of the contacts (sample output below):
