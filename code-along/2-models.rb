8860d62e1bfd49c642daa1abf44efde16bea0a4f
Julia Gangemi <julia.gangemi@kellogg.northwestern.edu>
2025-04-29 15:27:10 +0000

    learning steps in 2-models.rb

--- code-along/2-models.rb
+++ code-along/2-models.rb
@@ -16,11 +16,11 @@ Company.destroy_all
 
 # 2. insert new rows in companies table
 
-no_of_companies = Companies.all.count
+no_of_companies = Company.all.count
 
 puts "Companies: #{no_of_companies}"
 new_company = Company.new
-puts new_company.inspect
+# puts new_company.inspect
 new_company["name"] = "Apple"
 new_company["city"] = "Cupertino"
 new_company["state"] = "CA"
@@ -46,15 +46,38 @@ new_company["url"] = "https://airbnb.com"
 new_company.save
 # p new_company
 
-no_of_companies = Companies.all.count
+no_of_companies = Company.all.count
 puts "Companies: #{no_of_companies}"
 
 # 3. query companies table to find all row with California company
+cali_companies = Company.where({"state" => "CA"})
+p cali_companies
+
+puts "There are #{cali_companies.count} Cali companies"
 
 # 4. query companies table to find single row for Apple
+    # apple = Company.where({"name" => "Apple"})[0]
+    # below does the same as above but without having to insert the specific array row
+    # BUT if there are multiple rows with name = Apple, 
+    # it will only return the first row that it finds
+apple = Company.find_by({"name" => "Apple"})
 
 # 5. read a row's column value
+p apple
+p apple["name"]
+p apple["id"]
+p apple["city"]
 
 # 6. update a row's column value
+apple ["url"] = "https://www.apple.com"
+p apple
+apple.save
 
 # 7. delete a row
+airbnb = Company.find_by({"name" => "AirBnB"})
+p airbnb
+airbnb.destroy
+p airbnb
+
+no_of_companies = Company.all.count
+puts "Companies: #{no_of_companies}"
